ccb4d7da2b40b5432c9bb9849ede960a148977ea
gautam <gautampal560@gmail.com>
2023-01-18 20:58:29 +0530

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -46,7 +46,7 @@ const Feed = () => {
                     variant="body2"
 
                     sx={{ mt: 1.5, color: "#fff", }} >
-                    Copyright 2022 Vaibhav Jadhavar
+                    Copyright 2022 Gautam Pal
                 </Typography>
             </Box>
 
